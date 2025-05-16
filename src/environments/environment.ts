b74a6240fc50460093719d3588e42279ed1282d6
Andrei Sebald <asebald@google.com>
2025-05-16 15:53:47 -0700

    codelab starting point

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyA-Yeb-ABN5O966Um8XmfUObFPDEpCSGbQ",
+  authDomain: "csrm-nova-dev.firebaseapp.com",
+  projectId: "csrm-nova-dev",
+  storageBucket: "csrm-nova-dev.firebasestorage.app",
+  messagingSenderId: "142069853402",
+  appId: "1:142069853402:web:678908a04e607e971d89e3",
+  measurementId: "G-C2N3N4CYGG"
+  },
+};
